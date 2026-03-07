6f4e2cefd91d6c59dbafb3b25c30418e2f537109
Thunderquacker <165667407+Thunderquacker@users.noreply.github.com>
2026-03-07 23:31:51 +0800

    Create FILO Array Stack.c

---
+++ Stack/FILO Array Stack.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#define MAX 20 
+
+typedef struct{
+    char elem[MAX];
+    int top;
+}SA;
+
+void init(SA *s);
+bool isFull(SA s);
+bool isEmpty(SA s);
+void read(SA s);
+void push(SA *s, int data);
+char pop(SA *s);
+int top(SA s);
+
+int main(){
+    
+    SA s;
+    
+    init(&s);
+    
+    printf("Pushing: ");
+    push(&s, 'a');
+    push(&s, 'b');
+    read(s);
+    
+    printf("Popped: ");
+    pop(&s);
+    read(s);
+    
+    
+    
+    return 0;
+}
+
+void init(SA *s){
+    s->top = MAX;
+}
+
+bool isEmpty(SA s){
+    return (s.top == MAX) ? true : false;
+}
+
+bool isFull(SA s){
+    return (s.top == 0) ? true : false;
+}
+
+void push(SA *s, int data){
+    if(!isFull(*s)){
+        s->elem[--(s->top)] = data;
+    }
+}
+
+char pop(SA *s){
+    if(!isEmpty(*s)){
+        return s->elem[(s->top)++];
+    }
+}
+
+void read(SA s){
+    
+    if(isEmpty(s)){
+        printf("Stack is empty.");
+        return;
+    }
+    
+    for(int i = s.top; i < MAX; i++){
+        printf("%c", s.elem[i]);
+        
+        if(i < MAX - 1){
+            printf(", ");
+        } else {
+            printf(".\n");
+        }
+    }
+}
+
+int top(SA s){
+    if(s.top < MAX){
+    return s.elem[s.top];
+    }
+}
