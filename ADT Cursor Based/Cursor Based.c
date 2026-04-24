3bda8cf96a226d6c81771b781efd3dca61d21788
Thunderquacker <165667407+Thunderquacker@users.noreply.github.com>
2026-04-24 13:29:14 +0800

    Update Cursor Based.c

--- ADT Cursor Based/Cursor Based.c
+++ ADT Cursor Based/Cursor Based.c
@@ -6,7 +6,7 @@
 typedef struct{
     int data;
     int next;
-}Cell, HeapSpace[MAX];
+}HeapSpace[MAX];
 
 typedef struct{
     HeapSpace H;
@@ -15,13 +15,15 @@ typedef struct{
 
 typedef int List;
 
-
 void init(VHeap *VH);
 int allocSpace(VHeap *VH);
 void dealloc(VHeap *VH, int index);
+void display(VHeap VH, List L);
 void insertFirst(VHeap *VH, List *L, int data);
 void deleteFirst(VHeap *VH, List *L);
-void display(VHeap VH, List L);
+void insertLast(VHeap *VH, List *L, int data);
+void insertSorted(VHeap *VH, List *L, int data);
+void deleteAllOccurences(VHeap *VH, List *L, int data);
 
 
 int main(){
@@ -29,9 +31,10 @@ int main(){
     VHeap V;
     List L = -1;
     
-    init(&V);
     
-    insertFirst(&V, &L, 1);
+    init(&V);
+    insertFirst(&V, &L, 10);
+    insertLast(&V, &L, 2);
     display(V, L);
     
     
@@ -41,56 +44,83 @@ int main(){
 
 void init(VHeap *VH){
     
-    int i;
-    
-    for(i = 0; i < MAX - 1; i++){
+    for(int i = 0; i < MAX - 1; i++){
         VH->H[i].next = i + 1;
     }
-    
     VH->H[MAX - 1].next = -1;
     VH->avail = 0;
 }
 
 int allocSpace(VHeap *VH){
     
-    int res = VH->avail;
-    if(res != -1){
-        VH->avail = VH->H[res].next;
+    int index = VH->avail;
+    
+    if(index != -1){
+        VH->avail = VH->H[index].next;
     }
-    return res;
+    return index;
 }
 
+void dealloc(VHeap *VH, int index){
+    
+    VH->H[index].next = VH->avail;
+    VH->avail = index;
+}
 
 void insertFirst(VHeap *VH, List *L, int data){
+    
     int newNode = allocSpace(VH);
+    
     if(newNode != -1){
-        VH->H[newNode].data = data;
+       VH->H[newNode].data = data;
         VH->H[newNode].next = *L;
         *L = newNode;
     }
 }
 
-void deleteFirst(VHeap *VH, List *L){
+void insertLast(VHeap *VH, List *L, int data){
     
-    if(*L != -1){
+    int newNode = allocSpace(VH);
+    
+    VH->H[newNode].data = data;
+    VH->H[newNode].next = -1;
+    
+    if(*L == -1){
+        *L = newNode;
+    } else {
         int temp = *L;
-        *L = VH->H[temp].next;
-        dealloc(VH, temp);
+        while(VH->H[temp].next != -1){
+            temp = VH->H[temp].next;
+        }
+        VH->H[temp].next = newNode;
     }
 }
 
-void dealloc(VHeap *VH, int index){
+void insertSorted(VHeap *VH, List *L, int data){
     
-    if(index >= 0 && index < MAX){
-        VH->H[index].next = VH->avail;
-        VH->avail = index;
+    int newNode = allocSpace(VH);
+    
+    VH->H[newNode].data = data;
+    
+    if(*L == -1 || VH->H[*L].data >= data){
+        VH->H[newNode].next = *L;
+        *L = newNode;
+        return;
     }
+    
+    int curr = *L;
+    while(VH->H[curr].next != -1 && VH->H[VH->H[curr].next].data < data){
+        curr = VH->H[curr].next;
+    }
+    VH->H[newNode].next = VH->H[curr].next;
+    VH->H[curr].next = newNode;
 }
 
 void display(VHeap VH, List L){
+    int temp = L;
     
-    while(L != -1){
-        printf("%d", VH.H[L].data);
-        L = VH.H[L].next;
+    while(temp != -1){
+        printf("%d ", VH.H[temp].data);
+        temp = VH.H[temp].next;
     }
 }
